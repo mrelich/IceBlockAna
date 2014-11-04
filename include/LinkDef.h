e791d737f70ae0233fbcf64553dfa0c52521bdb6
mrelich <mrelich6@gmail.com>
2014-11-04 17:46:36 +0900

    Adding makefile copied over

---
+++ include/LinkDef.h
@@ -0,0 +1,16 @@
+#ifdef __CINT__
+
+#pragma link off all global;
+#pragma link off all class;
+#pragma link off all function;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ class Event;
+#pragma link C++ class Antenna;
+#pragma link C++ class MetaData;
+
+#pragma link C++ class std::vector<Antenna>;
+
+#endif
