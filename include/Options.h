ebd8dd51d3b680adb940e304b28d397bb45ef263
mrelich <mrelich6@gmail.com>
2014-11-06 14:43:08 +0900

    Adding options

---
+++ include/Options.h
@@ -0,0 +1,19 @@
+#ifndef Options_h
+#define Options_h
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+// Specify the constants that are going to be used //
+// in the experiment.  For example fixing the beam //
+// parameters.                                     //
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+
+namespace Options
+{
+
+  const int NParticles = 1e9; // 10^9 particles in one shot
+  const int NBunches   = 5;   // Number of bunches
+
+
+};
+
+#endif
